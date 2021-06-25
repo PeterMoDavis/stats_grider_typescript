03859ad98eb4fb93f809c58f23b35958c041442c
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-25 14:18:40 -0400

    implemented an enum

--- build/index.js
+++ build/index.js
@@ -12,4 +12,20 @@ var matches = fs_1.default
     .map(function (row) {
     return row.split(",");
 });
-console.log(matches);
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["Homewin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult.Homewin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Man United won " + manUnitedWins + " games");
