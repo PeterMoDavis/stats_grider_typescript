b8bfa669249fb1433ae11fa53936416aee540832
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-27 13:49:48 -0400

    first refactor of the CsvFileReader

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,7 +1,9 @@
 import { CsvFileReader } from "./CsvFileReader";
 import { dateStringToDate } from "./utils";
 
-export class MatchReader extends CsvFileReader {
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
+export class MatchReader extends CsvFileReader<MatchData> {
   mapRow(row: string[]): MatchData {
     return [
       dateStringToDate(row[0]),
